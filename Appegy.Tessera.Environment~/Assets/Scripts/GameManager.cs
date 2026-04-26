bbcdea5c9bed63e03914bb9656a0fd48e39c0244
Ivan Murashka <imurashka.me@gmail.com>
2026-04-27 00:20:35 +0200

    add environment Unity project (copied from BinaryPrefs, BinaryPrefs->Tessera grep replace)

---
+++ Appegy.Tessera.Environment~/Assets/Scripts/GameManager.cs
@@ -0,0 +1,58 @@
+﻿using System.IO;
+using UnityEngine;
+
+namespace Appegy.Storage.Example
+{
+    public class GameManager : MonoBehaviour
+    {
+        private BinaryStorage _storage;
+
+        private void Awake()
+        {
+            Debug.Log("Game started");
+            _storage = BinaryStorage
+                .Construct(Path.Combine(Application.persistentDataPath, "PlayerPrefs.bin"))
+                .AddPrimitiveTypes()
+                .SupportListsOf<int>()
+                .SupportListsOf<string>()
+                .EnableAutoSaveOnChange()
+                .Build();
+
+            _storage.GetListOf<int>("ints");
+            _storage.Remove("ints");
+
+            using (_storage.MultipleChangeScope())
+            {
+                var value = _storage.Get<int>("int_val", 0);
+                _storage.Set("int_val", value + 1);
+
+                using (_storage.MultipleChangeScope())
+                {
+                    value = _storage.Get<int>("int_val", 0);
+                    _storage.Set("int_val", value + 1);
+
+                    using (_storage.MultipleChangeScope())
+                    {
+                        value = _storage.Get<int>("int_val", 0);
+                        _storage.Set("int_val", value + 1);
+                    }
+                }
+            }
+        }
+
+        private void OnGUI()
+        {
+            var value = _storage.Get<int>("int_val", 0);
+            if (GUILayout.Button($"INT={value}"))
+            {
+                _storage.Set("int_val", value + 1);
+            }
+        }
+
+        private void OnDestroy()
+        {
+            _storage?.Dispose();
+            _storage = null;
+        }
+    }
+}
