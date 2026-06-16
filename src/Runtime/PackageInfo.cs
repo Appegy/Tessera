6f969f421a5d53e6d1977ae9ecb3b13bce582403
Ivan Murashka <imurashka.me@gmail.com>
2026-06-16 17:07:15 +0200

    ci: add release tooling - bump-version, auto-notes, PackageInfo (#18)

---
+++ src/Runtime/PackageInfo.cs
@@ -0,0 +1,12 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Appegy.Tessera.Tests")]
+
+namespace Appegy.Tessera
+{
+    public static class PackageInfo
+    {
+        public const string Name = "com.appegy.tessera";
+        public const string Version = "1.0.0";
+    }
+}
